1eac3d483524d875ebb19035d86f40463c115040
Karine Costa <karine.costa@somagrupo.com.br>
2023-04-22 21:01:12 -0300

    tira foto da webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import React, { useCallback, useState } from 'react';
+import Webcam from "react-webcam";
 import './App.css'
 
 function App() {
+  const ref = React.useRef() as React.MutableRefObject<Webcam>;
+  const [photoCapture, setPhotoCapture] = useState<string | undefined>();
+
+  const capture = useCallback(() => {
+    const makeScreenshot = ref.current.getScreenshot();
+    if (makeScreenshot)
+      setPhotoCapture(makeScreenshot)
+  }, [])
 
   return (
     <>
+      <img src={photoCapture} alt="foto" />
+      <Webcam
+        //espelhar imagem
+        mirrored={true}
+        screenshotFormat="image/jpeg"
+        ref={ref}
+      />
+      <button onClick={capture}>Tirar foto</button>
     </>
   )
 }
